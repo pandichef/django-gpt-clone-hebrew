3ae3e74d3b51c883d15b3944f4f6823a0ce46cb5
pandichef <rashtyda@gmail.com>
2023-10-31 00:49:33 -0500

    pandichef first commit

--- openai_django/base_app/oai_queries.py
+++ openai_django/base_app/oai_queries.py
@@ -12,31 +12,42 @@ openai.api_key = settings.OPENAI_API_KEY
 
 def get_completion(prompt):
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        # model=os.environ["OPENAI_MODEL_NAME"],
+        # model="gpt-3.5-turbo",
+        model=os.environ["OPENAI_MODEL_NAME"],
         messages=[
-            {"role": "system", "content": "translate from English to Hebrew",},
+            {
+                "role": "system",
+                "content": "translate English to Biblical Hebrew with Cantillation",
+            },
             {"role": "user", "content": prompt},
         ],
     )
-    he0 = completion.choices[0].message["content"]
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        # model=os.environ["OPENAI_MODEL_NAME"],
-        messages=[
-            {"role": "system", "content": "now add vowels to this text",},
-            {"role": "user", "content": he0},
-        ],
-    )
-    he1 = completion.choices[0].message["content"]
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        # model=os.environ["OPENAI_MODEL_NAME"],
-        messages=[
-            {"role": "system", "content": "now add cantillation symbols to this text",},
-            {"role": "user", "content": he1},
-        ],
-    )
-    he2 = completion.choices[0].message["content"]
-    # return completion.choices[0].message["content"]
-    return he2
+    # completion = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     # model=os.environ["OPENAI_MODEL_NAME"],
+    #     messages=[
+    #         {"role": "system", "content": "translate from English to Hebrew",},
+    #         {"role": "user", "content": prompt},
+    #     ],
+    # )
+    # he0 = completion.choices[0].message["content"]
+    # completion = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     # model=os.environ["OPENAI_MODEL_NAME"],
+    #     messages=[
+    #         {"role": "system", "content": "now add vowels to this text",},
+    #         {"role": "user", "content": he0},
+    #     ],
+    # )
+    # he1 = completion.choices[0].message["content"]
+    # completion = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     # model=os.environ["OPENAI_MODEL_NAME"],
+    #     messages=[
+    #         {"role": "system", "content": "now add cantillation symbols to this text",},
+    #         {"role": "user", "content": he1},
+    #     ],
+    # )
+    # he2 = completion.choices[0].message["content"]
+    # return he2
+    return completion.choices[0].message["content"]
