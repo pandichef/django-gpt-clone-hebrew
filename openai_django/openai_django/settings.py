b088ab0c05172ad5001bb6d9e2a6fb88c2ff2c7c
pandichef <rashtyda@gmail.com>
2023-11-02 11:19:25 -0500

    change for cantillation

--- openai_django/openai_django/settings.py
+++ openai_django/openai_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "gptheb.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost", "gptheb.pythonanywhere.com", "taam.pythonanywhere.com"]
 
 
 # Application definition
