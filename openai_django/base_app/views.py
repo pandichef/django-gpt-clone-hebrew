0f3c8e5c1eec8fe3fd708a10ad3d297752a358b4
pandichef <rashtyda@gmail.com>
2023-11-02 14:28:02 -0500

    change for cantillation

--- openai_django/base_app/views.py
+++ openai_django/base_app/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .oai_queries import get_completion
 
+
 def query_view(request):
-    if request.method == 'POST':
-        prompt = request.POST.get('prompt')
-        response = get_completion(prompt)
-        return JsonResponse({'response': response})
-    return render(request, 'query.html')
+    if request.method == "POST":
+        prompt = request.POST.get("prompt")
+        try:
+            response = get_completion(prompt)
+            return JsonResponse({"response": response})
+        except Exception as e:
+            return JsonResponse({"response": str(e)})
+    return render(request, "query.html")
